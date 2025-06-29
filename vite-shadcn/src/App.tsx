0a1f2cd6e0d2a50231e9edd2c911366ada1961e0
sandeepdhanni <sandeepdhanni703@gmail.com>
2025-06-29 14:51:50 +0530

    working on shadcn

---
+++ vite-shadcn/src/App.tsx
@@ -0,0 +1,11 @@
+import LoginPage from "./app/login/Page"
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App
